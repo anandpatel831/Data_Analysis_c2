f749fcb30a5aef707d48894dd29ff24b25e7abc5
Anand Patel <anandpatel82906@gmail.com>
2026-09-11 11:27:21 +0530

    Fix 10 Sep folder tracking

---
+++ C2/10_Sep_2026/num1.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+v = np.zeros(12)
+v[5] = 1
+print(v)
+# create a vector with value ranging from 10 to 100
+b=np.arange(10,100)
+print(b)
+
+b=np.arange(10,19).reshape(3,3)
+print(b)
